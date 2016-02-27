91b5ee673a17635ccf110f45133fba547822b63b
Sbomabast <Sbomabast@outlook.com>
2016-02-27 15:08:25 +0200

    Update Main Drive.cpp

--- Tut_2a/Main Drive.cpp
+++ Tut_2a/Main Drive.cpp
@@ -4,8 +4,22 @@ using namespace std;
 
 int main()
 {
+	int a;
+	int b;
+	int c;
+	int d;
+	cout << "Please enter the numerators and denominators of the two fractions to be manipulated\n" << endl;
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	cin >> d;
 	Fraction Frac_1;
 	Fraction Frac_2;
 	Fraction ResultingFrac;
-
+	ResultingFrac.add(Frac_1.a, Frac_1.b, Frac_2.c, Frac_2.d);
+	ResultingFrac.subtract(Frac_1.a, Frac_1.b, Frac_2.c, Frac_2.d);
+	ResultingFrac.mutliply(Frac_1.a, Frac_1.b, Frac_2.c, Frac_2.d);
+	ResultingFrac.divide(Frac_1.a, Frac_1.b, Frac_2.c, Frac_2.d);
+	ResultingFrac.print();
+	return 0;
 }
