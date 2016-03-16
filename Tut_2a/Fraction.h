1af57e7afcb15ecef89e4e6429879a37e44ae556
Sbomabast <Sbomabast@outlook.com>
2016-03-16 13:19:56 +0200

    Update Fraction.h

--- Tut_2a/Fraction.h
+++ Tut_2a/Fraction.h
@@ -5,14 +5,11 @@ private:
 	int num;
 	int denom;
 public:
-	Fraction(); // costructor
-	Fraction(); // destructor
-	int getNum(); // accesor for numerator
-	int getDenom(); // accesor for denominator
-	void setNum(int); // mutator for numerator
-	void setDenom(int); // mutator for denominator
-	int add(int, int, int, int);
-	int subtract(int, int, int, int);
+	Fraction(); 
+	~Fraction();
+	int getNumDenom(int &, int &); 
+	void setNumDenom(int, int);
+	void add(Fraction);
 	int multiply(int, int, int, int);
 	int divide(int, int, int, int);
 	void print();
